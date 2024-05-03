aca90eecd73ab8e3b31f7c6b678f0405b68543bf
vishalmanglaw453 <vishalmanglaw@gmail.com>
2024-05-03 13:50:56 +0530

    done

--- src/componets/Task.jsx
+++ src/componets/Task.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import emailjs from "@emailjs/browser";
 import Swal from "sweetalert2";
 const Task = () => {
-  const [value, setValue] = useState({
+  const [inputvalue, setValue] = useState({
     first: "",
     second: "",
     third: "",
@@ -12,11 +12,16 @@ const Task = () => {
   });
 
   const addvalue = (e) => {
-    setValue({ ...value, [e.target.name]: e.target.value });
+    setValue({ ...inputvalue, [e.target.name]: e.target.value });
   };
   function fromsubmit(e) {
     e.preventDefault();
-    console.log(value);
+    setValue(
+    {first: "",
+    second: "",
+    third: "",
+    four: "",
+    five: "",})
     Swal.fire({
       title: "Good job!",
       text: "Your task has been added !",
@@ -34,78 +39,85 @@ const Task = () => {
         console.error("Email failed to send!", error.text);
       }
     );
+   
   }
+  
   return (
     <>
       <div className="container d-flex justify-content-center">
         <div className="heaving_card">
-            <h2 className=" text-center">Email js</h2>
+          <h2 className=" text-center">Email js</h2>
 
-        <form onSubmit={fromsubmit}>
-          <div className="  d-flex gap-3">
-            <div className=" input_parent">
-              <label htmlFor="" className=" text-black  ">
-                Name*
-              </label>
-              <input
-                type="text"
-                value={value.first}
-                name="first"
-                onChange={addvalue}
-              />
+          <form onSubmit={fromsubmit}>
+            <div className="  d-flex gap-3">
+              <div className=" input_parent">
+                <label htmlFor="" className=" text-black  ">
+                  Name*
+                </label>
+                <input
+                  required
+                  type="text"
+                  value={inputvalue.first}
+                  name="first"
+                  onChange={addvalue}
+                />
+              </div>
+              <div className=" input_parent">
+                <label htmlFor="" className=" text-black  ">
+                  Email address*
+                </label>
+                <input
+                  required
+                  type="email"
+                  value={inputvalue.second}
+                  name="second"
+                  onChange={addvalue}
+                />
+              </div>
             </div>
-            <div className=" input_parent">
-              <label htmlFor="" className=" text-black  ">
-                Email address*
-              </label>
-              <input
-                type="text"
-                value={value.second}
-                name="second"
-                onChange={addvalue}
-              />
+            <div className="  d-flex gap-3">
+              <div className=" input_parent">
+                <label htmlFor="" className=" text-black  ">
+                  Phone number
+                </label>
+                <input
+                  required
+                  type="number"
+                  value={inputvalue.third}
+                  name="third"
+                  onChange={addvalue}
+                />
+              </div>{" "}
+              <div className=" input_parent">
+                <label htmlFor="" className=" text-black  ">
+                  Company*
+                </label>
+                <input
+                  required
+                  type="text"
+                  value={inputvalue.four}
+                  name="four"
+                  onChange={addvalue}
+                />
+              </div>
             </div>
-          </div>
-          <div className="  d-flex gap-3">
-            <div className=" input_parent">
-              <label htmlFor="" className=" text-black  ">
-                Phone number
-              </label>
-              <input
-                type="text"
-                value={value.third}
-                name="third"
-                onChange={addvalue}
-              />
-            </div>{" "}
-            <div className=" input_parent">
-              <label htmlFor="" className=" text-black  ">
-                Company*
-              </label>
-              <input
-                type="text"
-                value={value.four}
-                name="four"
-                onChange={addvalue}
-              />
+            <div className="  d-flex gap-3"></div>
+            <div className="  d-flex gap-3">
+              <div className=" input_parent">
+                <label htmlFor="" className=" text-black  ">
+                  Message*
+                </label>
+                <input
+                required
+                  type="text"
+                  value={inputvalue.five}
+                  name="five"
+                  onChange={addvalue}
+                />
+              </div>
             </div>
-          </div>
-          <div className="  d-flex gap-3"></div>
-          <div className="  d-flex gap-3">
-            <div className=" input_parent">
-              <label htmlFor="" className=" text-black  ">
-                Message*
-              </label>
-              <input
-                type="text"
-                value={value.five}
-                name="five"
-                onChange={addvalue}
-              />
-            </div>
-          </div>
-          <button className="touch_btn mt-4">GET IN TOUCH</button>
-        </form>
+            <button className="touch_btn mt-4">GET IN TOUCH</button>
+          </form>
         </div>
       </div>
     </>
